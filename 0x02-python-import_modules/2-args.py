d3c30a05860f69daafc65454489129d3557a195d
Rayd Trujillo <rayd1893@gmail.com>
2021-09-09 22:23:04 +0000

    Print arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-lenArg = len(sys.argv) - 1
-text = "arguments"
-sig = ":"
-argv = sys.argv
+    lenArg = len(sys.argv) - 1
+    text = "arguments"
+    sig = ":"
+    argv = sys.argv
 
-if lenArg == 0:
-    sig = "."
+    if lenArg == 0:
+        sig = "."
 
-if lenArg == 1:
-    text = "argument"
+    if lenArg == 1:
+        text = "argument"
 
-print("{:d} {}{}".format(lenArg, text, sig))
+    print("{:d} {}{}".format(lenArg, text, sig))
 
-for i in range(1, lenArg + 1):
-    print("{:d}: {}".format(i, argv[i]))
+    for i in range(1, lenArg + 1):
+        print("{:d}: {}".format(i, argv[i]))
