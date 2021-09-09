58d2aec106108ac436e42b65904f18945fe094c8
Rayd Trujillo <rayd1893@gmail.com>
2021-09-09 22:16:05 +0000

    Print arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import sys
+
+lenArg = len(sys.argv) - 1
+text = "arguments"
+sig = ":"
+argv = sys.argv
+
+if lenArg == 0:
+    sig = "."
+
+if lenArg == 1:
+    text = "argument"
+
+print("{:d} {}{}".format(lenArg, text, sig))
+
+for i in range(1, lenArg + 1):
+    print("{:d}: {}".format(i, argv[i]))
