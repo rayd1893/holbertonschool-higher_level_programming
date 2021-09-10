9b643a11e6c9a8e6c6cb8d88d84f6aa1a59fc56e
Rayd Trujillo <rayd1893@gmail.com>
2021-09-10 03:03:36 +0000

    Calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,6 +4,17 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
+    array = []
+
+    if (sys.argv[2])[-2:] == "py":
+        sys.argv[2] = "*"
+        sys.argv[3] = sys.argv[len(sys.argv) - 1]
+        array = sys.argv
+
+        for i in range(4, len(sys.argv)):
+            array.pop(4)
+        sys.argv = array
+
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
