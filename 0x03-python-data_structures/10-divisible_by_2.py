a4b228c6e79395ce2dd73f74d958e5387a7bac64
Rayd Trujillo <rayd1893@gmail.com>
2021-09-14 01:14:04 +0000

    Is divisible

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+def divisible_by_2(my_list=[]):
+    new_list = []
+
+
+    for i in my_list:
+        valor = False
+        if i % 2 == 0:
+            valor = True
+        new_list.append(valor)
+
+    return new_list
