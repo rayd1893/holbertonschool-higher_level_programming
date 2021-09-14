ac319b10f0e3cbe29ce191531b7cd65352edaae4
Rayd Trujillo <rayd1893@gmail.com>
2021-09-14 03:05:36 +0000

    Palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - Validate if list is palindrome
+ * @head: List
+ *
+ * Return: 1 if is palindrome or 0 if not palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t **h;
+	int i = 0, n = 0;
+	int *numbers;
+
+	h = head;
+
+	while (!*h)
+	{
+		*numbers = (*h)->n;
+		*h = (*h)->next;
+		n++;
+		numbers++;
+	}
+
+	while (i < n / 2)
+	{
+		if (numbers[i] != numbers[n - i - 1])
+			return (0);
+	}
+
+	return (1);
+}
