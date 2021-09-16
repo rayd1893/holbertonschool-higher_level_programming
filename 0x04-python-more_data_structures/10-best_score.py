feebbed139f530569df4f1dc16d3b93559f3c3a8
Rayd Trujillo <rayd1893@gmail.com>
2021-09-16 02:03:39 +0000

    Best Score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,9 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+    for i in a_dictionary.values():
+        if i is None:
+            return None
     order = sorted(a_dictionary.values())
     best = order[-1]
     for k, v in a_dictionary.items():
