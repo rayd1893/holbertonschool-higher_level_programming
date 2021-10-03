4476ac8e956b7d6a1d0ca1e3e344894eb2dbeeae
Rayd Trujillo <rayd1893@gmail.com>
2021-10-03 16:24:58 +0000

    Add two values

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+"""
+Task: 0. Integers addition
+Test: ./tests/0-add_integer.txt
+Module: 0-add_integer
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Function add_integer: Add two integers
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
