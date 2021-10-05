e5ed106e00d29e2fc3117abf8cb30b8c2eca5a27
Rayd Trujillo <rayd1893@gmail.com>
2021-10-05 02:01:45 +0000

    Update function

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         '''Defined constructor'''
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def __str__(self):
