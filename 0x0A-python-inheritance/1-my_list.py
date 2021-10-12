174382229df5796ee18ff2aa91dfca6c9c02bc1f
Rayd Trujillo <rayd1893@gmail.com>
2021-10-12 01:59:10 +0000

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,12 +6,6 @@
 
 class MyList(list):
     '''Class MyList inheret object list'''
-    def __init__(self):
-        '''Define Constructor'''
-        self.__newList = []
-
     def print_sorted(self):
         '''Print list sorted'''
-        self.__newList = self.copy()
-        self.__newList.sort()
-        print(self.__newList)
+        print(sorted(self))
