4f3fa064e7da66bb168dcf339e537d2aa11a8c45
Rayd Trujillo <rayd1893@gmail.com>
2021-10-18 23:57:53 +0000

    Add test in class rectangle

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -10,9 +10,18 @@ class TestRectangle(unittest.TestCase):
     '''Class test of Rectangle'''
 
     def test_id(self):
+        '''Test id'''
         r1 = Rectangle(10, 2)
         id = r1.id
         self.assertEqual(id, 2)
         r3 = Rectangle(10, 2, 0, 0, 10)
         id = r3.id
         self.assertEqual(id, 10)
+
+    def test_validate(self):
+        '''Validate integer'''
+        self.assertRaises(TypeError, Rectangle, 10, "2")
+        self.assertRaises(TypeError, Rectangle, "q", 3)
+        self.assertRaises(ValueError, Rectangle, 10, 0)
+        self.assertRaises(ValueError, Rectangle, -2, 6)
+        self.assertRaises(ValueError, Rectangle, 9, 6, 1, -1)
