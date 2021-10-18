c695d89d5bdbc6ab818aace430fe06b896130726
Rayd Trujillo <rayd1893@gmail.com>
2021-10-18 19:57:12 +0000

    Add comments in functions

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,7 +7,9 @@ from models.base import Base
 
 class Rectangle(Base):
     '''Class Rectangle'''
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        '''Define constructor'''
         super().__init__(id)
         self.width = width
         self.height = height
@@ -15,6 +17,7 @@ class Rectangle(Base):
         self.y = y
 
     def __str__(self):
+        '''Define function str'''
         x = self.__x
         y = self.__y
         width = self.__width
@@ -28,41 +31,50 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        '''Get width'''
         return self.__width
 
     @width.setter
     def width(self, width):
+        '''Set width'''
         self.validator_integer("width", width, 1)
         self.__width = width
 
     @property
     def height(self):
+        '''Get height'''
         return self.__height
 
     @height.setter
     def height(self, height):
+        '''Set height'''
         self.validator_integer("height", height, 1)
         self.__height = height
 
     @property
     def x(self):
+        '''Get x'''
         return self.__x
 
     @x.setter
     def x(self, x):
+        '''Set x'''
         self.validator_integer("x", x, 2)
         self.__x = x
 
     @property
     def y(self):
+        '''Get y'''
         return self.__y
 
     @y.setter
     def y(self, y):
+        '''Set y'''
         self.validator_integer("y", y, 2)
         self.__y = y
 
     def validator_integer(self, name, value, flag):
+        '''Validate integer'''
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if flag == 1:
