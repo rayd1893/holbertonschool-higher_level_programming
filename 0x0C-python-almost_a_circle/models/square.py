6ab69ce722695d543e714bca63d04f09e564133e
Rayd Trujillo <rayd1893@gmail.com>
2021-10-19 03:47:20 +0000

    Add class Square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Model Square'''
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+        '''Class Square'''
+
+        def __init__(self, size, x=0, y=0, id=None):
+             super().__init__(size, size, x, y, id)
