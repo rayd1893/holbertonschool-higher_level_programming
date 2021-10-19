987f80a256ad370fd7749bb074bfad92f34a45c8
Rayd Trujillo <rayd1893@gmail.com>
2021-10-19 04:27:45 +0000

    update class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,14 +9,14 @@ class Square(Rectangle):
         '''Class Square'''
 
         def __init__(self, size, x=0, y=0, id=None):
-                self.size = size
+                '''Define constructor'''
                 super().__init__(size, size, x, y, id)
 
         def __str__(self):
                 '''Define function str'''
                 x = self.x
                 y = self.y
-                size = self.__size
+                size = self.width
                 form = type(self).__name__
                 txt = "[" + form + "] "
                 txt += "(" + str(self.id) + ") "
@@ -27,10 +27,10 @@ class Square(Rectangle):
         @property
         def size(self):
                 '''Get size'''
-                return self.__size
+                return self.width
 
         @size.setter
         def size(self, size):
                 '''Set size'''
-                self.validator_integer("width", size, 1)
-                self.__size = size
+                self.width = size
+                self.height = size
