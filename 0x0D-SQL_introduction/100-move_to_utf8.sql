7761cac81d9365127a87a70c72bb70592df8ec32
Rayd Trujillo <rayd1893@gmail.com>
2021-11-16 20:11:15 +0000

    Go to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,15 @@
+-- Change collation in database
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- Change collation in table
+ALTER TABLE hbtn_0c_0.first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- Change collation in field
+ALTER TABLE hbtn_0c_0.first_table
+MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
