add618161ed092dddeaeecead95d022d707c01d2
Rayd Trujillo <rayd1893@gmail.com>
2021-11-16 20:34:30 +0000

    Change collation

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -13,5 +13,5 @@ COLLATE utf8mb4_unicode_ci;
 -- Change collation in field
 ALTER TABLE first_table
 MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+COLLATE utf8mb4_unicode_ci
+NULL;
