211cea980f0314728b2fb105070c30d4a42997f0
Rayd Trujillo <rayd1893@gmail.com>
2021-11-17 02:30:57 +0000

    Change collation

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,12 +5,10 @@ COLLATE utf8mb4_unicode_ci;
 
 use hbtn_0c_0;
 
--- Change collation in table
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
--- Change collation in field
 ALTER TABLE first_table
 MODIFY name VARCHAR(256)
 COLLATE utf8mb4_unicode_ci
