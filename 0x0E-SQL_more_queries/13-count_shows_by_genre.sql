fd7d42b50059794b71f7bf7475ae2d5a9341e1ff
Rayd Trujillo <rayd1893@gmail.com>
2021-11-18 02:49:43 +0000

    Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- inner join
-SELECT c.name as genre, COUNT(*) as number_of_shows
+SELECT c.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_shows a
 INNER JOIN tv_show_genres b
 ON a.id = b.show_id
