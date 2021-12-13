d9037a19644b3a2463381303787de3c5e49bebb6
Rayd Trujillo <rayd1893@gmail.com>
2021-12-13 19:48:32 +0000

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-import { argv } from 'process';
 
-if (argv[3]) {
+if (process.argv[3]) {
   console.log('Arguments found');
-} else if (argv[2]) {
+} else if (process.argv[2]) {
   console.log('Argument found');
 } else {
   console.log('No argument');
