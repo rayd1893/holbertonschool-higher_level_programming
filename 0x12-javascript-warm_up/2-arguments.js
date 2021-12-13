9e24dec16389e4a4c58827607ded04092b42baa2
Rayd Trujillo <rayd1893@gmail.com>
2021-12-13 19:38:18 +0000

    Add semistandar style

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 import { argv } from 'process';
 
-if(argv[2])
-{
-    console.log('Argument found');
-}
-else
-{
-    console.log('No argument');
+if (argv[2]) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
 }
