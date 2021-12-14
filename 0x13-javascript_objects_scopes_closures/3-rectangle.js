599131f632b4a3fb5e60fc705d34bdae76caa37f
Rayd Trujillo <rayd1893@gmail.com>
2021-12-14 22:46:57 +0000

    Add function print

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,15 +5,15 @@ class Rectangle {
     if (validate(w) !== undefined && validate(h) !== undefined) {
       this.width = w;
       this.height = h;
-      this.print = function () {
-        const text = 'X';
-        let i = 0;
-        while (i < this.height) {
-          console.log(text.repeat(this.width));
-          i++;
-        }
-      };
     }
+    this.print = function () {
+      const text = 'X';
+      let i = 0;
+      while (i < this.height) {
+        console.log(text.repeat(this.width));
+        i++;
+      }
+    };
   }
 }
 
