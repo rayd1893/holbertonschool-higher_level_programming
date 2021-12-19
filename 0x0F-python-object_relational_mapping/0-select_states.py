dadaaf83a40a65bb46a6d06603b26a043e735510
rayd1893 <rayd1893@gmail.com>
2021-12-19 15:06:59 -0500

    Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+    import MySQLdb
+
+    localhost = "localhost"
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(
+        host="localhost", user=username, passwd=password, db=database)
+    c = db.cursor()
+    query = "SELECT * "
+    query += "FROM states "
+    query += "ORDER BY 1"
+    c.execute(query)
+    registers = c.fetchall()
+    for i in registers:
+        print(i)
