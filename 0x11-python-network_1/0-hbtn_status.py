a8aa1f3f8ac9c444dde77cb012c36580d3aac56f
rayd1893 <rayd1893@gmail.com>
2022-01-09 12:51:50 -0500

    Add pycodostyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,14 +2,17 @@
 '''Print status of a request to a url'''
 import urllib.request
 
+
 def response_url():
-    '''Response of a url''' 
+    '''Response of a url'''
+
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(content)))
         print('\t- content: {}'.format(content))
         print('\t- utf8 content: {}'.format(content.decode('utf-8')))
+
 
 if __name__ == "__main__":
     response_url()
