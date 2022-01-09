d123fc73c9ea3c988b714ffa95c377d1701ca5e5
rayd1893 <rayd1893@gmail.com>
2022-01-09 12:25:13 -0500

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(content.__class__))
+        print('\t- content: {}'.format(content))
+        print('\t- utf8 content: {}'.format(content.decode('utf-8')))
