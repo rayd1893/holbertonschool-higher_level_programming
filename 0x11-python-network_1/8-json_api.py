fa93eac219f2d676525c75fb025a2c2b5202d156
rayd1893 <rayd1893@gmail.com>
2022-01-09 22:26:14 -0500

    Search json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Show json using requests'''
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        q = argv[1]
+    except Exception as e:
+        q = ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    data = r.json()
+    if type(data) is dict and len(data) != 0:
+        print('[{}] {}'.format(data['id'], data['name']))
+    else:
+        if len(data) == 0:
+            print('No result')
+        if type(data) is not dict:
+            print('Not a valid JSON')
