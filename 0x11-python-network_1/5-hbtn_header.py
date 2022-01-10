604c75edd4d173c6f6f53c3f6ca0056a1bcff692
rayd1893 <rayd1893@gmail.com>
2022-01-09 21:07:44 -0500

    Get headers using requests

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Show headers using requests'''
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    r = requests.get(url)
+    print(r.headers.get('X-Request-Id'))
