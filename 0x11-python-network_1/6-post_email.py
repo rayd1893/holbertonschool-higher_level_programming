a96a10b5855e1d6be3415ca11507d87f7a0be362
rayd1893 <rayd1893@gmail.com>
2022-01-09 21:25:41 -0500

    Method POST

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-'''Method POST using requests'''
+"""sends a POST request to the passed URL with the email as a parameter"""
 import requests
 from sys import argv
 
-
-if __name__ == '__main__':
-    url = argv[1]
-    r = requests.post(url, data={'email': argv[2]})
+if __name__ == "__main__":
+    r = requests.post(argv[1], data={'email': argv[2]})
     print(r.text)
