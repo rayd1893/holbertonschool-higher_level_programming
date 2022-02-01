ccdae957433c3ae44e86ff15369985f059818cee
rayd1893 <rayd1893@gmail.com>
2022-02-01 14:00:14 -0500

    Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const file = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(file, content, 'utf8', function (error) {
+  if (error) {
+    console.log(error);
+  }
+});
