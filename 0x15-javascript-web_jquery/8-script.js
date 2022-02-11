6e5a43c846aac2fc57b49bb6199764ac178deba7
Rayd Trujillo <rayd1893@gmail.com>
2022-02-10 22:24:11 -0500

    Star Wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+    for (const i of data.results) {
+      $('UL#list_movies').append('<li>' + i.title + '</li>');
+    }
+  });
+});
